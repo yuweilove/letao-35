b2a636044a1e56955b3e81654686eecda8d27a9d
yuweilove <2914669017@qq.com>
2019-02-22 20:40:14 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -11,23 +11,29 @@ $(function(){
                     notEmpty: {
                         message: '用户名不能为空'
                       },
-                      stringLength: {
+                    stringLength: {
                         min: 2,
                         max: 6,
                         message: '用户名长度必须在6到30之间'
                       },
+                    callback:{
+                      message: '用户名不存在'
+                    }
                 }
             },
             password:{
                 validators:{
                     notEmpty: {
-                        message: '用户名不能为空'
+                        message: '密码不能为空'
                     },
                       stringLength: {
                         min: 6,
                         max: 30,
-                        message: '用户名长度必须在6到30之间'
+                        message: '密码长度必须在6到30之间'
                     },
+                    callback: {
+                      message: '密码错误'
+                    }
                 } 
             }
         }
@@ -42,10 +48,10 @@ $(function(){
             success:function(info){
                 console.log(info);
                 if(info.error===1000){
-                  alert ('用户名错误');
+                  $('#form').data('bootstrapValidator').updateStatus( 'username', 'INVALID', 'callback' );
                 }
                 if(info.error===1001){
-                  alert ('密码错误');
+                  $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
                 }
                 if(info.success){
                   location.href='index.html';
